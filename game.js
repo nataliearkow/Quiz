955ae2ede814512542be595363cd64df0bc12f84
Natalie Arkow <109115351+nataliearkow@users.noreply.github.com>
2022-09-10 12:37:54 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,112 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'How many states are in the United States?',
+        choice1: '49',
+        choice2: '50',
+        choice3: '52',
+        choice4: '51',
+        answer: 2,
+    },
+    {
+        question: 'What is the biggest state in the U.S.?',
+        choice1: 'Texas',
+        choice2: 'New Mexico',
+        choice3: 'Alaska',
+        choice4: 'California',
+        answer: 3,
+    },
+    {
+        question: 'Which state is known as "The Big Apple?"',
+        choice1: 'Connecticut',
+        choice2: 'Massachusetts',
+        choice3: 'New Jersey',
+        choice4: 'New York',
+        answer: 4,
+    },
+    {
+        question: 'Which state is known as the "Lone Star State?"',
+        choice1: 'Texas',
+        choice2: 'New York',
+        choice3: 'Washington',
+        choice4: 'Alabama',
+        answer: 1,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
